43ae7aaf2b2e5a91dcecd8254759954a2f5b8e25
ekcless <cashmoneyhilinski@gmail.com>
2025-01-15 20:20:30 -0800

    fix3

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/no-unescaped-entities': 0,
+    '@next/next/no-unescaped-entities': 0
+  }
+} 
